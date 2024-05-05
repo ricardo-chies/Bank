dc83e9fa3fdea30c274be0942363eab4bad098c6
Ricardo chies <ricardochies5@gmail.com>
2024-05-05 13:01:11 -0300

    Feat: Logs Controller Usuário

--- Microsservico.Bancario/Bancario.API/Controllers/UsuariosController.cs
+++ Microsservico.Bancario/Bancario.API/Controllers/UsuariosController.cs
@@ -1,5 +1,6 @@
 using Bancario.Application.Dtos;
 using Bancario.Application.Interfaces;
+using Bancario.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -11,34 +12,66 @@ namespace Bancario.API.Controllers
     public class UsuariosController : Controller
     {
         private readonly IUsuarioService _service;
+        private readonly ILogger<UsuariosController> _logger;
 
-        public UsuariosController(IUsuarioService service)
+        public UsuariosController(IUsuarioService service, ILogger<UsuariosController> logger)
         {
             _service = service;
+            _logger = logger;
         }
 
         [HttpPost]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         public async Task<IActionResult> CriarUsuario([FromBody] UsuarioDto usuario)
         {
-            var resultado = await _service.CriarUsuario(usuario);
-            return Ok(resultado);
+            try
+            {
+                _logger.LogInformation("[CriarUsuario] Criando novo usuário: {usuario.Cpf}", usuario.Cpf);
+                var resultado = await _service.CriarUsuario(usuario);
+                return Ok(resultado);
+            } 
+            catch (Exception ex)
+            {
+                _logger.LogInformation("[CriarUsuario] Não foi possível criar o usuário: {usuario.Cpf} - Exception Message: {ex}", usuario.Cpf, ex);
+                return StatusCode(500, new ErrorDto("Não foi possível criar o usuário", ex.Message, ex.StackTrace ?? ""));
+            }
+
         }
 
         [HttpGet("{cpf}")]
         [ProducesResponseType(typeof(UsuarioDto), StatusCodes.Status200OK)]
         public async Task<IActionResult> ObterUsuarioPorCpf([FromRoute] string cpf)
         {
-            var usuario = await _service.ObterUsuarioPorCpf(cpf);
-            return Ok(usuario);
+            try
+            {
+                _logger.LogInformation("[ObterUsuarioPorCpf] Buscando usuário: {cpf}", cpf);
+                var usuario = await _service.ObterUsuarioPorCpf(cpf);
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("[ObterUsuarioPorCpf] Não foi possível localizar o usuário: {cpf} - Exception Message: {ex}", cpf, ex);
+                return StatusCode(500, new ErrorDto("Não foi possível localizar o usuário", ex.Message, ex.StackTrace ?? ""));
+            }
+
         }
 
         [HttpGet("login")]
         [ProducesResponseType(typeof(UsuarioDto), StatusCodes.Status200OK)]
         public async Task<IActionResult> LoginUsuario([FromQuery] string cpf, string senha)
         {
-            var resultado = await _service.LoginUsuario(cpf, senha);
-            return Ok(resultado);
+            try
+            {
+                _logger.LogInformation("[LoginUsuario] Verficando credenciais usuário: {cpf}", cpf);
+                var resultado = await _service.LoginUsuario(cpf, senha);
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("[LoginUsuario] Não foi possível realizar o login de usuário: {cpf} - Exception Message: {ex}", cpf, ex);
+                return StatusCode(500, new ErrorDto("Não foi possível realizar o login de usuário", ex.Message, ex.StackTrace ?? ""));
+            }
+
         }
     }
 }
---
+++ Microsservico.Bancario/Bancario.Application/Dtos/ErrorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Bancario.Application.Dtos
+{
+    public class ErrorDto
+    {
+        public string Erro { get; set; }
+        public string Message { get; set; }
+        public string Stack { get; set; }
+
+        public ErrorDto(string erro, string message, string stack)
+        {
+            this.Erro = erro;
+            this.Message = message;
+            this.Stack = stack;
+        }
+    }
+}
